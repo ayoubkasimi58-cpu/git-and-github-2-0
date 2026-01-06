ace30bc5196267ee5c6d25abb9aa883849d1f1d1
ayoub el kasmi <ayoubkasimi58@gmail.com>
2026-01-06 09:49:00 +0000

    describe your change

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("btn").addEventListener("click", () => {
+    document.getElementById("text").innerText =
+      "Merci de votre intérêt ! Ensemble, nous construisons le futur 🚀";
+  });
+  
